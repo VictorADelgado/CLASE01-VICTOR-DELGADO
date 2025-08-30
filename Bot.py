63605a64d7e4dce327d0739c1f6d10e563296112
VictorADelgado <vianrodelcla2@gmail.com>
2025-08-29 21:22:07 -0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,11 +1,11 @@
 #Dictionary app
 word = input("Hola! Soy un bot de diccionario de palabras interesantes.¿Hay alguna palabra que no entiendes? \n")
 word_dict = {
-    "LOL":"una respuesta a algo gracioso";
-    "CRINGE":"algo raro o embarazoso";
-    "ROFL":"una respuesta a una broma";
-    "SHEESH":"ligera desaprobación";
-    "CREEPY":"aterrador, siniestro";
+    "LOL":"una respuesta a algo gracioso",
+    "CRINGE":"algo raro o embarazoso",
+    "ROFL":"una respuesta a una broma",
+    "SHEESH":"ligera desaprobación",
+    "CREEPY":"aterrador, siniestro",
     "AGGRO":"ponerse agresivo o enojado"
 }
 if word in word_dict.keys():
